c30549c4960b4bfd949fc42aca9d4653e260e088
Jayson <jaysonneoh@gmail.com>
2023-07-29 00:51:18 +0800

    Updated initial code in BrowseTab, GetLatestAlbums in BrowseBloc, BrowseEvent, BrowseState

--- lib/ui/home_page/tabs/browse_tab/bloc/browse_event.dart
+++ lib/ui/home_page/tabs/browse_tab/bloc/browse_event.dart
@@ -1,17 +1,9 @@
 part of 'browse_bloc.dart';
 
 
-abstract class BrowseEvent extends Equatable {
-  const BrowseEvent();
-
+class BrowseEvent extends Equatable {
   @override
   List<Object?> get props => [];
 }
 
-class FetchAlbumsEvent extends BrowseEvent {
-  // Event to trigger fetching albums from the AlbumService
-}
-
-class FetchArtistsEvent extends BrowseEvent {
-  // Event to trigger fetching artists from the ArtistService
-}
+class GetLatestAlbums extends BrowseEvent {}
