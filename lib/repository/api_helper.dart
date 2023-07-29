9c835d0df1bb81ae9506234cb3ba33015d4d4218
Jayson <jaysonneoh@gmail.com>
2023-07-30 01:11:42 +0800

    Moved subUrl to APIHelper, Corrected response parsing

--- lib/repository/api_helper.dart
+++ lib/repository/api_helper.dart
@@ -4,6 +4,13 @@ import 'package:dio/dio.dart';
 
 class APIHelper {
   final String _baseUrl = 'https://api.spotify.com/v1';
+  final String newReleaseSubUrl = '/browse/new-releases';
+  final String albumSubUrl = '/albums';
+  final String categoriesSubUrl = '/browse/categories';
+  final String featuredPlaylistsSubUrl = '/browse/featured-playlists';
+  final String playlistsSubUrl = '/playlists';
+  final String artistsSubUrl = '/artists';
+
   late Dio _dio;
   late Map<String, dynamic> _header;
 
