0507dec1381bbe1bd76edec65ff96efc90b6f8c7
Jayson <jaysonneoh@gmail.com>
2023-07-30 13:51:08 +0800

    Added CategoryIconInfo in Category

--- lib/model/category.dart
+++ lib/model/category.dart
@@ -1,13 +1,20 @@
+import 'package:apple_music_clone/model/category_icon_info.dart';
+
+
 class Category {
   final String id;
   final String name;
+  final List<CategoryIconInfo> categoryIconsInfo;
 
-  Category(this.id, this.name);
+  Category(this.id, this.name, this.categoryIconsInfo);
 
   factory Category.fromMap(Map <String, dynamic> respData){
+    List<CategoryIconInfo> icons = (respData['icons'] as List?)?.map<CategoryIconInfo>((iconMap) => CategoryIconInfo.fromMap(iconMap)).toList() ?? [];
+
     return Category(
         respData['id'] ?? '',
-        respData['name'] ?? ''
+        respData['name'] ?? '',
+        icons
     );
   }
 
