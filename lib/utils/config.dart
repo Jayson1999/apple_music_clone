68f94056b80bc966a9e0b46db4b93cad1829e050
Jayson <jaysonneoh@gmail.com>
2023-07-30 23:49:25 +0800

    Added TextSizes in config

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -3,3 +3,9 @@ import 'dart:ui';
 const String clientId = '';
 const String clientSecret = '';
 final String localRegion = window.locale.countryCode ?? '';
+
+class TextSizes {
+  static const double big = 24.0;
+  static const double medium = 15.0;
+  static const double small = 9.0;
+}
