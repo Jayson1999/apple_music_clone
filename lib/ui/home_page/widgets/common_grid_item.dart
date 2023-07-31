fd2bbeac6adf317f8a09d3050a757c4048b2d3d8
Jayson <jaysonneoh@gmail.com>
2023-07-31 18:42:15 +0800

    Added CommonGridItem

--- lib/ui/home_page/widgets/common_grid_item.dart
+++ lib/ui/home_page/widgets/common_grid_item.dart
@@ -2,10 +2,12 @@ import 'package:apple_music_clone/utils/config.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
-Widget commonGridItem(BuildContext context, String headerButtonTitle, int noOfRows, List<String> titleList, List<String> subtitleList, List<String> imgUrlList) {
-  for (int i=0; i<noOfRows; i++){
 
-  }
+Widget commonGridItem(BuildContext context, String headerButtonTitle, int noOfRows, List<String> titleList, List<String> subtitleList, List<String> imgUrlList) {
+  final int noOfItemsPerRow = titleList.length~/noOfRows;
+  List<List<String>> splitTitleLists = _splitList(titleList, noOfItemsPerRow);
+  List<List<String>> splitSubtitleLists = _splitList(subtitleList, noOfItemsPerRow);
+  List<List<String>> splitImgUrlLists = _splitList(imgUrlList, noOfItemsPerRow);
 
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
@@ -20,21 +22,30 @@ Widget commonGridItem(BuildContext context, String headerButtonTitle, int noOfRo
         ),
       ),
       SizedBox(
-        height: MediaQuery.of(context).size.height * 0.3,
+        height: MediaQuery.of(context).size.height * 0.3 * noOfRows,
         child: PageView.builder(
-            controller: PageController(viewportFraction: 0.9),
+            padEnds: false,
+            controller: PageController(viewportFraction: 0.5),
             scrollDirection: Axis.horizontal,
-            itemCount: titleList.length,
-            itemBuilder: (context, index) {
-              return _singleCardItem(context, titleList[index], subtitleList[index], imgUrlList[index]);
-            }
-        ),
-      ),
+            itemCount: noOfItemsPerRow,
+            itemBuilder: (context, pageIndex) {
+              List<Widget> currentPageCards = [
+                for (int rowIndex=0; rowIndex<noOfRows; rowIndex++)
+                  _singleCardItem(
+                      context,
+                      splitTitleLists[rowIndex][pageIndex],
+                      splitSubtitleLists[rowIndex][pageIndex],
+                      splitImgUrlLists[rowIndex][pageIndex]
+                  )
+              ];
+              return Column(children: currentPageCards);
+            }),
+      )
     ],
   );
 }
 
-Widget _singleCardItem(BuildContext context, String title, String subtitle, String imgUrl){
+Widget _singleCardItem(BuildContext context, String title, String subtitle, String imgUrl) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
@@ -60,10 +71,13 @@ Widget _singleCardItem(BuildContext context, String title, String subtitle, Stri
       ),
       Padding(
         padding: const EdgeInsets.only(left: 8.0),
-        child: Text(title, style: const TextStyle(fontSize: TextSizes.medium, color: Colors.black),),
+        child: Text(
+          title,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(fontSize: TextSizes.medium, color: Colors.black),),
       ),
       Padding(
-        padding: const EdgeInsets.only(left: 8.0),
+        padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
         child: Text(
           subtitle,
           overflow: TextOverflow.ellipsis,
@@ -72,4 +86,12 @@ Widget _singleCardItem(BuildContext context, String title, String subtitle, Stri
       ),
     ],
   );
+}
+
+List<List<T>> _splitList<T>(List<T> list, int size) {
+  List<List<T>> result = [];
+  for (int i = 0; i < list.length; i += size) {
+    result.add(list.sublist(i, i + size > list.length ? list.length : i + size));
+  }
+  return result;
 }
