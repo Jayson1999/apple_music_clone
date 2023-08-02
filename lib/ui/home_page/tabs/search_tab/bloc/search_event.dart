85a2384667ac3c546588fa3f25de9a0ca9b4e204
Jayson <jaysonneoh@gmail.com>
2023-08-02 23:06:12 +0800

    Added Search and its bloc, event, state

---
+++ lib/ui/home_page/tabs/search_tab/bloc/search_event.dart
@@ -0,0 +1,10 @@
+part of 'search_bloc.dart';
+
+
+class SearchEvent extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class GetUserSubscription extends SearchEvent {}
+class GetCategoriesPlaylists extends SearchEvent {}
