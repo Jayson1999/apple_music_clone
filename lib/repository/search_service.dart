0ead74a25356a1d5cf1b8703eac97d918cce48eb
Jayson <jaysonneoh@gmail.com>
2023-08-05 01:01:07 +0800

    Updated Search UI, updated search Event & State, added Search Service, added search subUrl

---
+++ lib/repository/search_service.dart
@@ -0,0 +1,52 @@
+import 'dart:developer';
+import 'package:apple_music_clone/model/album.dart';
+import 'package:apple_music_clone/model/artist.dart';
+import 'package:apple_music_clone/model/playlist.dart';
+import 'package:apple_music_clone/model/track.dart';
+import 'package:apple_music_clone/repository/api_helper.dart';
+
+
+class SearchService {
+  final APIHelper _apiHelper;
+  final List<String> allowedSearchTypes = ['album', 'artist', 'playlist', 'track'];
+
+  SearchService(this._apiHelper);
+
+  Future<List> getSearchResults(String queryStr, {String country = '', int limit = 5}) async {
+    Map<String, dynamic> queryParams = {
+      'q': queryStr,
+      'type': allowedSearchTypes.join(','),
+      'limit': limit,
+    };
+    if (country.isNotEmpty){
+      queryParams['market'] = country;
+    }
+
+    try {
+      await _apiHelper.updateAuthorizationHeader();
+      final response = await _apiHelper.dio.get(_apiHelper.searchSubUrl, queryParameters: queryParams);
+
+      if (response.statusCode != 200){
+        throw Exception('$response');
+      }
+
+      List albumResponse = response.data['albums']?['items'] ?? [];
+      List artistResponse = response.data['artists']?['items'] ?? [];
+      List playlistResponse = response.data['playlists']?['items'] ?? [];
+      List trackResponse = response.data['tracks']?['items'] ?? [];
+
+      List<Album> albums = [for (Map<String, dynamic> albumData in albumResponse) Album.fromMap(albumData)];
+      List<Artist> artists = [for (Map<String, dynamic> artistData in artistResponse) Artist.fromMap(artistData)];
+      List<Playlist> playlists = [for (Map<String, dynamic> playlistData in playlistResponse) Playlist.fromMap(playlistData)];
+      List<Track> tracks = [for (Map<String, dynamic> trackData in trackResponse) Track.fromMap(trackData)];
+
+      return [...albums, ...artists, ...playlists, ...tracks];
+    }
+    catch (error, stack) {
+      final String errorMsg = 'GetSearchResults failed: $error\n$stack';
+      log(errorMsg, error: 'ERROR', name: 'GetSearchResults');
+      throw Exception(errorMsg);
+    }
+  }
+
+}
--- lib/ui/home_page/tabs/search_tab/bloc/search_state.dart
+++ lib/ui/home_page/tabs/search_tab/bloc/search_state.dart
@@ -12,84 +12,60 @@ extension SearchStatusX on SearchStatus {
 
 class SearchState extends Equatable {
   const SearchState({
-    this.status = SearchStatus.initial,
-    this.latestGlobalAlbums = const [],
-    this.latestLocalAlbums = const [],
-    this.featuredGlobalPlaylists = const [],
-    this.featuredLocalPlaylists = const [],
+    this.pageLoadStatus = SearchStatus.initial,
+    this.searchStatus = SearchStatus.initial,
     this.categoriesGlobalPlaylists = const [],
     this.categoriesLocalPlaylists = const [],
     this.categoriesGlobal = const [],
     this.categoriesLocal = const [],
-    this.artistsGlobal = const [],
-    this.artistsLocal = const [],
-    this.recommendedTracks = const[],
+    this.searchedResults = const[],
     this.userSubscription = 0,
     this.errorMsg = '',
   });
 
-  final SearchStatus status;
-  final List<Album> latestGlobalAlbums;
-  final List<Album> latestLocalAlbums;
-  final List<Playlist> featuredGlobalPlaylists;
-  final List<Playlist> featuredLocalPlaylists;
+  final SearchStatus pageLoadStatus;
+  final SearchStatus searchStatus;
   final List<List<Playlist>> categoriesGlobalPlaylists;
   final List<List<Playlist>> categoriesLocalPlaylists;
   final List<Category> categoriesGlobal;
   final List<Category> categoriesLocal;
-  final List<Artist> artistsGlobal;
-  final List<Artist> artistsLocal;
-  final List<Track> recommendedTracks;
+  final List searchedResults;
   final int userSubscription;
   final String errorMsg;
 
   @override
   List<Object?> get props => [
-    status,
-    latestGlobalAlbums,
-    latestLocalAlbums,
-    featuredGlobalPlaylists,
-    featuredLocalPlaylists,
+    pageLoadStatus,
+    searchStatus,
     categoriesGlobalPlaylists,
     categoriesLocalPlaylists,
     categoriesGlobal,
     categoriesLocal,
-    artistsGlobal,
-    artistsLocal,
-    recommendedTracks,
+    searchedResults,
     userSubscription,
     errorMsg
   ];
 
   SearchState copyWith(
-      {List<Album>? latestGlobalAlbums,
-        List<Album>? latestLocalAlbums,
-        List<Playlist>? featuredGlobalPlaylists,
-        List<Playlist>? featuredLocalPlaylists,
-        List<List<Playlist>>? categoriesGlobalPlaylists,
+      { List<List<Playlist>>? categoriesGlobalPlaylists,
         List<List<Playlist>>? categoriesLocalPlaylists,
         List<Category>? categoriesGlobal,
         List<Category>? categoriesLocal,
-        List<Artist>? artistsGlobal,
-        List<Artist>? artistsLocal,
-        List<Track>? recommendedTracks,
-        SearchStatus? status,
+        List? searchedResults,
+        SearchStatus? pageLoadStatus,
+        SearchStatus? searchStatus,
         int? userSubscription,
         String? errorMsg}) {
     return SearchState(
-        latestGlobalAlbums: latestGlobalAlbums ?? this.latestGlobalAlbums,
-        latestLocalAlbums: latestLocalAlbums ?? this.latestLocalAlbums,
-        featuredGlobalPlaylists: featuredGlobalPlaylists ?? this.featuredGlobalPlaylists,
-        featuredLocalPlaylists: featuredLocalPlaylists ?? this.featuredLocalPlaylists,
         categoriesGlobalPlaylists: categoriesGlobalPlaylists ?? this.categoriesGlobalPlaylists,
         categoriesLocalPlaylists: categoriesLocalPlaylists ?? this.categoriesLocalPlaylists,
         categoriesGlobal: categoriesGlobal ?? this.categoriesGlobal,
         categoriesLocal: categoriesLocal ?? this.categoriesLocal,
-        artistsGlobal: artistsGlobal ?? this.artistsGlobal,
-        artistsLocal: artistsLocal ?? this.artistsLocal,
-        recommendedTracks: recommendedTracks ?? this.recommendedTracks,
-        status: status ?? this.status,
+        searchedResults: searchedResults ?? this.searchedResults,
+        pageLoadStatus: pageLoadStatus ?? this.pageLoadStatus,
+        searchStatus: searchStatus ?? this.searchStatus,
         userSubscription: userSubscription ?? this.userSubscription,
         errorMsg: errorMsg ?? this.errorMsg);
   }
+
 }
