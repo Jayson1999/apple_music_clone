8dd4992330b0e9017471f944921b142dc1fa89e0
Jayson <jaysonneoh@gmail.com>
2023-08-08 13:56:18 +0800

    Refactored CircularCarousel & CircularItem into different classes

--- lib/ui/home_page/widgets/circular_item.dart
+++ lib/ui/home_page/widgets/circular_item.dart
@@ -1,85 +1,40 @@
-import 'package:apple_music_clone/ui/home_page/details_pages/artist_details/artist_details_page.dart';
-import 'package:apple_music_clone/ui/home_page/details_pages/artist_details/bloc/artist_bloc.dart';
-import 'package:apple_music_clone/ui/home_page/details_pages/expanded_artists_page.dart';
 import 'package:apple_music_clone/utils/config.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
+class CircularItem extends StatelessWidget {
+  const CircularItem({Key? key, required this.title, required this.imgUrl}) : super(key: key);
 
-Widget circularItem(BuildContext context, String headerButtonTitle, List dataList) {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      TextButton(
-        onPressed: () => Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => ArtistsExpandedPage(dataList: dataList, title: headerButtonTitle),
+  final String title;
+  final String imgUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 50,
+          child: ClipOval(
+            child: CachedNetworkImage(
+              fit: BoxFit.cover,
+              height: MediaQuery.of(context).size.height * 0.15,
+              width: double.infinity,
+              imageUrl: imgUrl,
+              placeholder: (context, url) => const Center(child: CircularProgressIndicator()),
+              errorWidget: (context, url, error) => const Center(child: Icon(Icons.error)),
+            ),
           ),
         ),
-        child: Row(
-          children: [
-            Text(headerButtonTitle, style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.black),),
-            const Icon(Icons.chevron_right, color: Colors.grey,)
-          ],
-        ),
-      ),
-      SizedBox(
-        height: MediaQuery.of(context).size.height * 0.2,
-        child: PageView.builder(
-            padEnds: false,
-            controller: PageController(viewportFraction: 0.32),
-            scrollDirection: Axis.horizontal,
-            itemCount: dataList.length,
-            itemBuilder: (context, pageIndex) {
-              var detailsPage = BlocProvider<ArtistBloc>(
-                create: (context) => ArtistBloc(),
-                child: ArtistDetailsPage(artist: dataList[pageIndex])
-              );
-              return InkWell(
-                onTap: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => detailsPage,
-                  ),
-                ),
-                child: _circularItem(
-                    context,
-                    dataList[pageIndex].name,
-                    dataList[pageIndex].images.first.url
-                ),
-              );
-            }),
-      )
-    ],
-  );
-}
-
-Widget _circularItem(BuildContext context, String title, String imgUrl) {
-  return Column(
-    children: [
-      CircleAvatar(
-        radius: 50,
-        child: ClipOval(
-          child: CachedNetworkImage(
-            fit: BoxFit.cover,
-            height: MediaQuery.of(context).size.height * 0.15,
-            width: double.infinity,
-            imageUrl: imgUrl,
-            placeholder: (context, url) => const Center(child: CircularProgressIndicator()),
-            errorWidget: (context, url, error) => const Center(child: Icon(Icons.error)),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 8.0),
+          child: Text(
+            title,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(fontSize: AppConfig.mediumText, color: Colors.black),
           ),
         ),
-      ),
-      Padding(
-        padding: const EdgeInsets.only(bottom: 8.0),
-        child: Text(
-          title,
-          overflow: TextOverflow.ellipsis,
-          style: const TextStyle(fontSize: AppConfig.mediumText, color: Colors.black),
-        ),
-      ),
-    ],
-  );
+      ],
+    );
+  }
+
 }
