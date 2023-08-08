0cf5c6fba9e32e0da1e4d84042c7cd6ba5bd1981
Jayson <jaysonneoh@gmail.com>
2023-08-08 21:21:57 +0800

    Added navigation in search suggestions

--- lib/ui/home_page/tabs/search_tab/search_delegate.dart
+++ lib/ui/home_page/tabs/search_tab/search_delegate.dart
@@ -2,6 +2,12 @@ import 'package:apple_music_clone/model/album.dart';
 import 'package:apple_music_clone/model/artist.dart';
 import 'package:apple_music_clone/model/playlist.dart';
 import 'package:apple_music_clone/model/track.dart';
+import 'package:apple_music_clone/ui/home_page/details_pages/album_details/album_details_page.dart';
+import 'package:apple_music_clone/ui/home_page/details_pages/album_details/bloc/album_bloc.dart';
+import 'package:apple_music_clone/ui/home_page/details_pages/artist_details/artist_details_page.dart';
+import 'package:apple_music_clone/ui/home_page/details_pages/artist_details/bloc/artist_bloc.dart';
+import 'package:apple_music_clone/ui/home_page/details_pages/playlist_details/bloc/playlist_bloc.dart';
+import 'package:apple_music_clone/ui/home_page/details_pages/playlist_details/playlist_details_page.dart';
 import 'package:apple_music_clone/ui/home_page/tabs/search_tab/bloc/search_bloc.dart';
 import 'package:apple_music_clone/ui/home_page/widgets/list_item.dart';
 import 'package:apple_music_clone/utils/config.dart';
@@ -70,11 +76,11 @@ class SearchBarDelegate extends SearchDelegate<String> {
                   final currentItem = state.searchedResults[index];
                   switch (currentItem.type){
                     case 'artist':
-                      return _singleArtist(currentItem);
+                      return _singleArtist(context, currentItem);
                     case 'album':
-                      return _singleAlbum(currentItem);
+                      return _singleAlbum(context, currentItem);
                     case 'playlist':
-                      return _singlePlaylist(currentItem);
+                      return _singlePlaylist(context, currentItem);
                     case 'track':
                       return _singleTrack(context, currentItem);
                   }
@@ -93,69 +99,96 @@ class SearchBarDelegate extends SearchDelegate<String> {
     }
   }
 
-  Widget _singleArtist(Artist artistData){
-    return ListTile(
-      leading: CircleAvatar(
-        radius: 15,
-        child: ClipOval(
-          child: CachedNetworkImage(
-            fit: BoxFit.cover,
-            imageUrl: artistData.images.isNotEmpty ? artistData.images[0].url : '',
-            errorWidget: (context, url, error) => const Center(child: Icon(Icons.error)),
+  Widget _singleArtist(BuildContext context, Artist artistData){
+    return InkWell(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => BlocProvider<ArtistBloc>(
+            create: (context) => ArtistBloc(),
+            child: ArtistDetailsPage(artist: artistData)
+        )),
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          radius: 15,
+          child: ClipOval(
+            child: CachedNetworkImage(
+              fit: BoxFit.cover,
+              imageUrl: artistData.images.isNotEmpty ? artistData.images[0].url : '',
+              errorWidget: (context, url, error) => const Center(child: Icon(Icons.error)),
+            ),
           ),
         ),
-      ),
-      title: Text(
-        artistData.name,
-        overflow: TextOverflow.ellipsis,
-        style: const TextStyle(fontSize: AppConfig.mediumText),
-      ),
-      subtitle: Text(
-       artistData.type,
-        overflow: TextOverflow.ellipsis,
-        style: const TextStyle(fontSize: AppConfig.smallText, color: Colors.grey),
-      )
-    );
-  }
-
-  Widget _singlePlaylist(Playlist playlistData){
-    return ListTile(
-      leading: CachedNetworkImage(
-        imageUrl: playlistData.images[0].url,
-        width: 30,
-        errorWidget: (context, url, error) =>
-        const Center(child: Icon(Icons.error)),
-      ),
-      title: Text(
-        playlistData.name,
-        overflow: TextOverflow.ellipsis,
-        style: const TextStyle(fontSize: AppConfig.mediumText),
-      ),
-      subtitle: Text(
-        '${playlistData.type} . ${playlistData.tracks.length} tracks',
-        overflow: TextOverflow.ellipsis,
-        style: const TextStyle(fontSize: AppConfig.smallText, color: Colors.grey),
-      )
-    );
-  }
-
-  Widget _singleAlbum(Album albumData){
-    return ListTile(
-        leading: CachedNetworkImage(
-          imageUrl: albumData.images.first.url,
-          width: 30,
-          errorWidget: (context, url, error) => const Center(child: Icon(Icons.error)),
-        ),
         title: Text(
-          albumData.name,
+          artistData.name,
           overflow: TextOverflow.ellipsis,
           style: const TextStyle(fontSize: AppConfig.mediumText),
         ),
         subtitle: Text(
-          '${albumData.type} . ${[for (Artist a in albumData.artists) a.name].join(',')}',
+         artistData.type,
           overflow: TextOverflow.ellipsis,
           style: const TextStyle(fontSize: AppConfig.smallText, color: Colors.grey),
         )
+      ),
+    );
+  }
+
+  Widget _singlePlaylist(BuildContext context, Playlist playlistData){
+    return InkWell(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => BlocProvider<PlaylistBloc>(
+            create: (context) => PlaylistBloc(),
+            child: PlaylistDetails(playlistId: playlistData.id)
+        )),
+      ),
+      child: ListTile(
+        leading: CachedNetworkImage(
+          imageUrl: playlistData.images[0].url,
+          width: 30,
+          errorWidget: (context, url, error) =>
+          const Center(child: Icon(Icons.error)),
+        ),
+        title: Text(
+          playlistData.name,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(fontSize: AppConfig.mediumText),
+        ),
+        subtitle: Text(
+          '${playlistData.type} . ${playlistData.tracks.length} tracks',
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(fontSize: AppConfig.smallText, color: Colors.grey),
+        )
+      ),
+    );
+  }
+
+  Widget _singleAlbum(BuildContext context, Album albumData){
+    return InkWell(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => BlocProvider<AlbumBloc>(
+            create: (context) => AlbumBloc(),
+            child: AlbumDetails(albumId: albumData.id,)
+        )),
+      ),
+      child: ListTile(
+          leading: CachedNetworkImage(
+            imageUrl: albumData.images.first.url,
+            width: 30,
+            errorWidget: (context, url, error) => const Center(child: Icon(Icons.error)),
+          ),
+          title: Text(
+            albumData.name,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(fontSize: AppConfig.mediumText),
+          ),
+          subtitle: Text(
+            '${albumData.type} . ${[for (Artist a in albumData.artists) a.name].join(',')}',
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(fontSize: AppConfig.smallText, color: Colors.grey),
+          )
+      ),
     );
   }
 
