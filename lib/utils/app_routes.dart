db5e72a785e6633885f3ea41bfbc0334b62f3392
Jayson <jaysonneoh@gmail.com>
2023-08-11 19:06:52 +0800

    Added AppRoutes, added details & expanded pages args class

---
+++ lib/utils/app_routes.dart
@@ -0,0 +1,14 @@
+class AppRoutes{
+  static const String homePage = '/home';
+  static const String firstPage = '/first';
+  static const String settingsPage = '/settings';
+  static const String albumDetailsPage = '/albumDetails';
+  static const String playlistDetailsPage = '/playlistDetails';
+  static const String artistDetailsPage = '/artistDetails';
+  static const String categoryDetailsPage = '/categoryDetails';
+  static const String albumsExpandedPage = '/albumsExpanded';
+  static const String playlistsExpandedPage = '/playlistsExpanded';
+  static const String artistsExpandedPage = '/artistsExpanded';
+  static const String categoriesExpandedPage = '/categoriesExpanded';
+  static const String tracksExpandedPage = '/tracksExpanded';
+}
